83f7767eeb0e77b4955c17869c980ef84b06aaa5
moveold0611 <137735075+moveold0611@users.noreply.github.com>
2023-11-27 15:48:56 +0900

    refactor OrderService

--- src/main/java/com/woofnmeow/wnm_project_back/service/OrderService.java
+++ src/main/java/com/woofnmeow/wnm_project_back/service/OrderService.java
@@ -62,27 +62,22 @@ public class OrderService {
 
     // R
    public List<GetUserOrdersRespDto> getOrdersForAdmin(SearchOrderReqDto searchOrderReqDto) {
-        List<GetUserOrdersRespDto> result = new ArrayList<>();
         try {
-            result = orderMapper.selectOrdersForAdmin(searchOrderReqDto.toVo(0)).stream().map(Order::toGetUserOrdersRespDto).collect(Collectors.toList());
+            return orderMapper.selectOrdersForAdmin(searchOrderReqDto.toVo(0)).stream().map(Order::toGetUserOrdersRespDto).collect(Collectors.toList());
         }catch (Exception e) {
-            Map<String, String> errorMap = new HashMap<>();
-            errorMap.put("주문 오류", "주문 조회 중 오류가 발생하였습니다.");
-            throw new OrderException(errorMap);
+            throw new OrderException
+                    (errorMapper.errorMapper("주문 오류", "주문 조회 중 오류가 발생하였습니다."));
         }
-        return result;
+        
     }
 
     public int getOrderCount(SearchOrderReqDto searchOrderReqDto) {
-        int count = 0;
         try {
-            count = orderMapper.getOrderCount(searchOrderReqDto.toVo(0));
+            return orderMapper.getOrderCount(searchOrderReqDto.toVo(0));
         }catch (Exception e) {
-            Map<String, String> errorMap = new HashMap<>();
-            errorMap.put("주문 오류", "주문 갯수 조회 중 오류가 발생하였습니다.");
-            throw new OrderException(errorMap);
+            throw new OrderException
+                    (errorMapper.errorMapper("주문 오류", "주문 갯수 조회 중 오류가 발생하였습니다."));
         }
-        return count;
     }
 
 
